74f3b9b3bfde25a1e8dc96b68c91cfc95bf1e6ff
Andrew Patzsch <mrpatzsch@gmail.com>
2015-06-05 13:56:18 -0700

    push to heroku

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,6 @@
 angular.module('starter.controllers', [])
 
+
 .controller('SignupCtrl', function($scope, $http, $state) {
   $scope.user = {username: "", company: "", address: "", address2: "", city: "", state: "", zipcode: "", tel: "", email: "", passwordDigest: ""};
   $scope.addUser = function() {
@@ -53,7 +54,15 @@ angular.module('starter.controllers', [])
       console.log("change detected:",newValue);
       $scope.association = newValue;
     });
+
+  $scope.$watch(function() {
+      return $scope.violations;
+    }, function(newValue, oldValue) {
+      console.log("change detected:",newValue);
+      $scope.violations = newValue;
+    });
   $scope.house = {address: "", street: ""};
+  $scope.violations = {address: "", street: ""};
   $http.get("http://localhost:3000/associations/" + $stateParams.name).
     success(function(d, stat, conf) {
       $scope.association = d;
@@ -65,7 +74,7 @@ angular.module('starter.controllers', [])
   $scope.addHouse = function() {
     console.log($scope.house); //Object {address: "some address", street: "some street"}
     console.log($scope.house.address, $scope.house.street);
-    $http.post("http://localhost:3000/associations/" + $scope.association.name, {address: $scope.house.address, street: $scope.house.street}).
+    $http.post("http://localhost:3000/associations/" + $scope.association.name, {address: $scope.house.address, street: $scope.house.street} + "/violations").
       success(function(data) {
         $scope.association = data;
         $scope.house = {address: "", street: ""};
@@ -74,6 +83,17 @@ angular.module('starter.controllers', [])
         console.log('error',data);
       });
     };
+
+  $scope.addViolation = function() {
+    console.log($scope.violations);
+    $http.post("http://localhost:3000/associations/" + $scope.association.name, {address: $scope.violations.address, street: $scope.violations.street}).
+      success(function(data) {
+        $scope.violations = data;
+      }).
+      error(function(data) {
+        console.log("error", data);
+      });
+  };
 });
 
 // .controller('ChatsCtrl', function($scope, Chats) {
