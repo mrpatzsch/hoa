a955dafe3985b78583a4ab325edbfed8f01c5602
Andrew Patzsch <mrpatzsch@gmail.com>
2015-06-25 10:23:34 -0700

    made it my own

--- www/js/services.js
+++ www/js/services.js
@@ -1,26 +1,34 @@
 angular.module('starter.services', [])
 
-/**
- * A simple example service that returns some data.
- */
-.factory('Friends', function() {
+.factory('Prices', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
-
+  var prices = [{
+    id: 0,
+    name: 'Essentials',
+    pricing: 0.50,
+    minimum: 100,
+    extra: 0.35,
+    image: 'ion-image'
+  }, {
+    id: 1,
+    name: 'Plus',
+    pricing: 0.75,
+    minimum: 250,
+    extra: 0.55,
+    image: 'ion-images'
+  },{
+    id: 2,
+    name: 'Premium',
+    pricing: 0.95,
+    minimum: 450,
+    extra: 0.75,
+    image: 'ion-android-camera'
+  }];
   return {
     all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
+      return prices;
     }
-  }
+  };
 });
